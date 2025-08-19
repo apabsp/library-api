7fabbc67b2dfe879feec78b83e0bdbf5a8d2f8bd
Antonio Paulo Barros <95260401+apabsp@users.noreply.github.com>
2025-08-19 17:21:23 -0300

    added get books req by specific author

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,10 @@ const router = Router(); // Use interno
 const pool = require("../connectdb");
 
 router.post("/", async (req, res) =>{
-  const {title, genre, author, year} = req.body; // title and other info will come from this request
+  const {name, genre, author, year} = req.body; // title and other info will come from this request
 
   const result = await pool.query(
-    "INSERT INTO books (title, genre, author, year) VALUES ($1, $2, $3, $4)RETURNING *", [title, genre, author, year] // * returns the complete row that was inserted
+    "INSERT INTO books (name, genre, author, year) VALUES ($1, $2, $3, $4)RETURNING *", [name, genre, author, year] // * returns the complete row that was inserted
   );
 
   res.status(201).json({result: result.rows[0]});
@@ -17,8 +17,16 @@ router.get("/", async (req, res) =>{
 
   const result = await pool.query("SELECT * FROM books",[]);
 
-  res.status(200).json({result: result});
-  //res.status(200).json({result: result.rows[0]});
-} )
+  //res.status(200).json({result: result});
+  res.status(200).json({result: result.rows});
+})
+
+router.get("/byAuthor", async(req,res) => {
+  const result = await pool.query("SELECT * FROM books WHERE author = ($1)",[req.body.author]);
+
+  res.status(200).json({result: result.rows});
+})
+
+
 
 module.exports = router;
